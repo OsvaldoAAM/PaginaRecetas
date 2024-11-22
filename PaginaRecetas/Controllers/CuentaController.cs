56b12ea962c6bdad8c0de24bc2190bab012911cd
Raúl Manzanera Medina <raul.manzaneram@uanl.edu.mx>
2024-11-22 17:32:18 -0600

    Se creo un controlador para gestionar las vistas de "Cuenta"

---
+++ PaginaRecetas/Controllers/CuentaController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PaginaRecetas.Controllers
+{
+    public class CuentaController : Controller
+    {
+        public IActionResult Cuenta()
+        {
+            return View();
+        }
+
+        public IActionResult RecetasPublicadas()
+        {
+            return View();
+        }
+
+        public IActionResult CrearReceta()
+        {
+            return View();
+        }
+
+        public IActionResult EditarReceta()
+        {
+            return View();
+        }
+    }
+}
