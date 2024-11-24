c7cf27fa93699b7339abab555840c87a861dbf03
OsvaldoAAM <arielomaac@gmail.com>
2024-11-23 21:47:10 -0600

    Preparaci+on para el add migration

--- PaginaRecetas/Program.cs
+++ PaginaRecetas/Program.cs
@@ -1,17 +1,18 @@
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
-using PaginaRecetas.Data;
+using PaginaRecetas.Models.dbModels;
+
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
-builder.Services.AddDbContext<ApplicationDbContext>(options =>
+builder.Services.AddDbContext<BdPrecetasContext>(options =>
     options.UseSqlServer(connectionString));
 builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 
 builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
-    .AddEntityFrameworkStores<ApplicationDbContext>();
+    .AddEntityFrameworkStores<BdPrecetasContext>();
 builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
