4b7e168317df1beea35e4b9d86b1656c6f56364f
Giovani040510 <giovaniarmandotorreshernandez@gmail.com>
2024-11-24 20:00:09 -0600

    pagina sobre agregada

--- PaginaRecetas/Controllers/HomeController.cs
+++ PaginaRecetas/Controllers/HomeController.cs
@@ -23,6 +23,11 @@ namespace PaginaRecetas.Controllers
             return View();
         }
 
+        public IActionResult sobre()
+        {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
